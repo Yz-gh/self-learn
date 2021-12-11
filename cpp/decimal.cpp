ac7c4763f82a7fe96e916f0087dd15d207c88afa
Yz-gh <75504671+Yz-gh@users.noreply.github.com>
2021-12-11 12:30:55 +0700

    Update decimal.cpp

--- cpp/decimal.cpp
+++ cpp/decimal.cpp
@@ -9,10 +9,26 @@ int main(int argc, char *argv[])
 	cout << "Masukkan nilai b : ";
 	cin >> b;
 	cout << endl;
-	if (b == 0)
-{
+	if (b == 0){
 		cout << "Bilangan pembagi tidak bisa 0";
 	}else{
 		cout << "Hasil pembagian " << a << " dan " << b << " adalah " << setprecision(4) << a/b << endl;
 	}
 }
+
+/* Cara 2
+#include <stdio.h>
+int main()
+{
+    float a, b;
+    printf("Masukkan nilai a : ");
+    scanf("%f", &a);
+    printf("Masukkan nilai b : ");
+    scanf("%f", &b);
+    printf("\n");
+    if (b == 0)
+        printf("%g dibagi dengan nol = TAK BERHINGGA\n", 
+a);
+    else
+    printf("%g dibagi dengan %g = %.3f\n", a, b, a/b);
+}*/
