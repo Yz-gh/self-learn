653f0452b0a4f5cc0a7aec0854e488998b653aeb
Yz-gh <75504671+Yz-gh@users.noreply.github.com>
2021-12-11 11:39:04 +0700

    Create p6.pas

---
+++ pascal/p6.pas
@@ -0,0 +1,25 @@
+program gaji;
+uses crt;
+var
+  gol, pdk: string;
+  gp, tj, gt: integer;
+begin
+  write('Input golongan karyawan : '); readln(gol);
+  write('Input pendidikan karyawan : '); readln(pdk);
+  gol := UpCase(gol);
+  pdk := UpCase(pdk);
+  if (gol = 'A') then
+    begin
+      if (pdk = 'SMA') then gp := 3000000
+      else gp := 4000000;
+    end
+  else
+    begin
+      if (pdk = 'SMA') then gp := 4000000
+      else gp := 6000000;
+    end;
+  tj := gp-1000000;
+  gt := gp+tj;
+  writeln('Gaji yang didapat : ',gt);
+  readln;
+end.
